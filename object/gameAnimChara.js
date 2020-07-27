3eb8c3659f796909a5154714ccbf8f42f92652e6
Fro <fromous@gmail.com>
2020-07-27 21:41:19 +0700

    gameanimchara class

--- object/gameAnimChara.js
+++ object/gameAnimChara.js
@@ -1,9 +1,28 @@
 class GameAnimChara extends Game {
-    constructor(player){
+    constructor(player, firstID, firstChara){
         super(player);
+        this.turnID = firstID;
+        this.turnChara = firstChara;
     }
 
-    answer(){
-        
+    answer(character){
+        this.bannedCharacter.push(character);
+    }
+
+    changeTurn(nextID, nextChara){
+        this.turnID = nextID;
+        this.turnChara = nextChara;
+    }
+
+    thisTurnID(){
+        return this.turnID;
+    }
+
+    thisTurnChara(){
+        return this.turnChara;
+    }
+
+    banned(){
+        return this.bannedCharacter;
     }
 }
