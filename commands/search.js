65ff5143cf5e9fa3f3291638dd0be07a814f54d9
FadhlikaAnggit <fadhlikanggit@gmail.com>
2020-07-29 09:39:52 +0700

    Search

--- commands/search.js
+++ commands/search.js
@@ -1,8 +1,5 @@
-const jikanjs  = require('jikanjs'); 
-const Discord = require('discord.js');
-const Jikan_node = require('jikan-node');
-
-mal = new Jikan_node(); 
+ const Discord = require('discord.js');
+const request = require('request');
 
 module.exports = {
 	name: 'search',
@@ -18,65 +15,69 @@ module.exports = {
 		}
 		for(var i = 1; i < args.length ; i++){
 			search = search + " " + args[i];
+			if(search.length < 3) {
+				msg.channel
+					.send( `The given query must be of minimum 3 letters! Given query '${search}' has only ${query.length} letters.` );
+				return
+			}
 		}
+		
 		msg.channel.send("You search for" + search);
-		type = "anime"
+		 
 		
-		var h = [];
-		mal.search(type, search, {page:1, limit:1})
-			.then((hasil) => {
-				console.log(hasil.results);
-			})
-			.catch(err=>console.log(err));
+		request('https://api.jikan.moe/v3/search/anime?q=Fate/Zero&page=1', function (error, response, body) {
+            console.log('Status:', response.statusCode);
+            console.log('Headers:', JSON.stringify(response.headers));
+            console.log('Response:', body);
+        });
+
+		
+		
+		jikanjs.search(type, search, '1', {limit : 10}).then((response) => {
+			if(response.results.length == 1){
+				if(type === "character"){
+					console.log(response.results);
+ 					msg.channel.send(response.results.name);
+					if(response.result[0].anime.length > 0){
+						msg.channel.send(response.results[0].anime[0].name);
+					}
+					if(response.result[0].manga.length > 0){
+						msg.channel.send(response.results[0].manga[0].name);
+					}
+				}else if(type === "anime"){
+					console.log(response.results.length);
+ 					msg.channel.send( response.results[0].title );
+ 					msg.channel.send( response.results[0].synopsis );
+				}else if(type ==="manga"){
+ 					msg.channel.send( response.results[0].title );
+ 					msg.channel.send( response.results[0].synopsis );
+				}
+			}else if(response.results.length == 0){
+				msg.channel.send(search + " Not Found in " + type);
+			}else if(response.results.length > 1 ){
+				response.results.forEach(element => {
+					if(type === "character"){
+ 						msg.channel.send( element.name );
+						if(element.anime.length > 0){
+							msg.channel.send( element.anime[0].name );		
+						}
+						if(element.manga.length > 0){
+							msg.channel.send( element.manga[0].name );	
+						}
+					}else if(type === "anime"){
+						console.log(response.results.length);
+ 						msg.channel.send( element.title );
+ 						msg.channel.send( element.synopsis );
+					}else if(type ==="manga"){
+						console.log(response.results.length);
+ 						msg.channel.send( element.title );
+ 						msg.channel.send( element.synopsis );
+					}
+				})
+			}
 			
-		console.log(h);
-		
-		
-		//jikanjs.search(type, search, '1', {limit : 10}).then((response) => {
-//			if(response.results.length == 1){
-//				if(type === "character"){
-//					console.log(response.results);
-// 					msg.channel.send(response.results.name);
-//					if(response.result[0].anime.length > 0){
-//						msg.channel.send(response.results[0].anime[0].name);
-//					}
-//					if(response.result[0].manga.length > 0){
-//						msg.channel.send(response.results[0].manga[0].name);
-//					}
-//				}else if(type === "anime"){
-//					console.log(response.results.length);
-// 					msg.channel.send( response.results[0].title );
-// 					msg.channel.send( response.results[0].synopsis );
-//				}else if(type ==="manga"){
-// 					msg.channel.send( response.results[0].title );
-// 					msg.channel.send( response.results[0].synopsis );
-//				}
-//			}else if(response.results.length == 0){
-//				msg.channel.send(search + " Not Found in " + type);
-//			}else if(response.results.length > 1 ){
-//				response.results.forEach(element => {
-//					if(type === "character"){
-// 						msg.channel.send( element.name );
-//						if(element.anime.length > 0){
-//							msg.channel.send( element.anime[0].name );		
-//						}
-//						if(element.manga.length > 0){
-//							msg.channel.send( element.manga[0].name );	
-//						}
-//					}else if(type === "anime"){
-//						console.log(response.results.length);
-// 						msg.channel.send( element.title );
-// 						msg.channel.send( element.synopsis );
-//					}else if(type ==="manga"){
-//						console.log(response.results.length);
-// 						msg.channel.send( element.title );
-// 						msg.channel.send( element.synopsis );
-//					}
-//				})
-//			}
-			
-		//}).catch((err) => {
-//			console.error(err);   //in case a error happens
-//		});
+		}).catch((err) => {
+			console.error(err);   //in case a error happens
+		});
 	},
 };
